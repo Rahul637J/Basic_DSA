bd94d220ba4a50fd38a060cc6a4bb3b9d1cda047
Your Name <you@example.com>
2024-09-29 01:50:06 +0530

    [Rahul] Add: Python program to create an intersection of sets

---
+++ Sets/6_Intersection_set.py
@@ -0,0 +1,25 @@
+'''
+@Author: Rahul 
+@Date: 2024-08-05
+@Last Modified by: Rahul 
+@Last Modified time: 2024-08-05
+@Title: Python program to create an intersection of sets    
+'''
+def interSection(set1,set2):
+          '''
+          Description: 
+              Description of function   
+          Parameters:
+              None 
+          Return :
+              None
+          '''
+          return set1&set2
+
+def main():
+    set1={1,2,3,4,5}
+    set2={2,5}
+    print(interSection(set1,set2))
+    
+if __name__=="__main__":
+    main()

940893b2561df299231f4872faf6c76b6c1e57cf
Your Name <you@example.com>
2024-09-29 01:19:54 +0530

    [Rahul] Add: Python program to manilpuate list

---
+++ Basic_Python/3_Display_in_list.py
@@ -0,0 +1,23 @@
+'''
+@Author: Rahul 
+@Date: 2024-07-30
+@Last Modified by: Rahul 
+@Last Modified time: 2024-07-30
+@Title: Python program to display the first and last colors from the list.    
+'''
+def findingDay():
+          '''
+          Description:
+              Prints the first and last elements of the given list of colours.
+          Parameters:
+              colours (list): A list of colour names (strings).
+          Return:
+              None
+          '''
+        
+def main():
+    colours=["Red","Green","White","Black"]
+    print(colours[0],colours[-1])
+    
+if __name__=="__main__":
+    main()

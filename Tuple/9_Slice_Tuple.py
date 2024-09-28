2914be97c766def544f3da269207ac4bbd760dab
Your Name <you@example.com>
2024-09-29 02:00:46 +0530

    [Rahul] Add: Python program to slice the tuple

---
+++ Tuple/9_Slice_Tuple.py
@@ -0,0 +1,14 @@
+'''
+@Author: Rahul 
+@Date: 2024-08-06
+@Last Modified by: Rahul 
+@Last Modified time: 2024-08-06
+@Title: Python program to slice the tuple    
+'''
+
+def main():
+    tuples=(1,2,3,4,5,6)
+    print(tuples[2:])
+    
+if __name__=="__main__":
+    main()

8cf9b4679014004e5c396debfff56f590d0501ed
Your Name <you@example.com>
2024-09-29 01:42:58 +0530

    [Rahul] Add: Python program to clone or copy a list

---
+++ List/7_Copy_or_Clone.py
@@ -0,0 +1,15 @@
+'''
+@Author: Rahul 
+@Date: 2024-08-05
+@Last Modified by: Rahul 
+@Last Modified time: 2024-08-05
+@Title: Python program to clone or copy a list    
+'''
+
+def main():
+    list1=[1,2,3,4,5,6]
+    list2=list(list1)
+    print(list2)
+    
+if __name__=="__main__":
+    main()

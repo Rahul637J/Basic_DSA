5b87b21fb8d70e5c513ff86e0fd852db2e83948a
Your Name <you@example.com>
2024-09-29 01:12:39 +0530

    [Rahul] Add: Python program to remove item(s) from a set

---
+++ Array/1_Create_Array.py
@@ -0,0 +1,26 @@
+'''
+@Author: Rahul 
+@Date: 2024-08-02
+@Last Modified by: Rahul 
+@Last Modified time: 2024-08-02
+@Title: Python program to create an array of 5 integers and display the array items    
+'''
+import array as arr
+def create_Array():
+          '''
+          Description: 
+            The function to create an array
+          Parameters:
+              None 
+          Return :
+              None
+          '''
+          a=arr.array('i',[1,2,3,4,5])
+          for i in range(len(a)):
+                print(a[i],end=" ")
+        
+def main():
+    create_Array()
+    
+if __name__=="__main__":
+    main()
